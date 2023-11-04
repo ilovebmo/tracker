a2b0a912290aca6882771b656533a1596bef01f0
gon <gon.ven.nunes@gmail.com>
2023-11-04 11:10:04 +0000

    rev_b fix

--- lib.py
+++ lib.py
@@ -5,6 +5,9 @@ Includes several useful functions and data/values for use in the UDPT.
 import ctypes, random, sys, os, pickle
 from socket import gethostbyname, gethostname
 
+# Reverses bits
+def rev_b(num) -> bytes:
+    return bytes(reversed(bytes(num)))
 
 # The Peer class definition
 class Peer:
@@ -102,9 +105,7 @@ def resource_path(relative_path):
 def gethost() -> str:
     return gethostbyname(gethostname())
 
-# Reverses bits
-def rev_b(num) -> bytes:
-    return bytes(reversed(bytes(num)))
+
 
 # Makes int32_t
 def make32(num):
