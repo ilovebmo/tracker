ed428e926b7a51bb1d042b98b456eb4a9de1f8d6
gon <gon.ven.nunes@gmail.com>
2023-11-04 20:17:42 +0000

    better naming

--- cli.bat
+++ cli.bat
@@ -1,3 +1,3 @@
-pyinstaller cli.py -D -F -n "gUDPT CLI" -i "util/icon.ico" --clean --distpath "."
+pyinstaller cli.py -D -F -n "gUDPT-CLI" -i "util/icon.ico" --clean --distpath "."
 del ".\gUDPT CLI.spec"
 pause
--- gui.bat
+++ gui.bat
@@ -1,3 +1,3 @@
-pyinstaller gui.py -D -F -n "gUDPT GUI" -w -i "util/icon.ico" --add-data "util/icon.ico;util"  --clean --distpath "."
+pyinstaller gui.py -D -F -n "gUDPT-GUI" -w -i "util/icon.ico" --add-data "util/icon.ico;util"  --clean --distpath "."
 del ".\gUDPT GUI.spec"
 pause
